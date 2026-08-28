f1a51ad5c8f1903b4efe5243ecd3c9180956046b
Punit Tiwan <tiwanpunit@gmail.com>
2026-08-28 17:09:40 -0400

    v2.2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='number-adventure-v24';
-const ASSETS=['./','./index.html','./styles.css?v=24','./game.js?v=24','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='number-adventure-v25';
+const ASSETS=['./','./index.html','./styles.css?v=25','./game.js?v=25','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
